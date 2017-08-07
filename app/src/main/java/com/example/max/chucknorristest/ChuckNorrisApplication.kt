f55bf9217f16d0f9454fe33b6e07a8adf50b17c9
Max Cruz <maxc@blanclabs.com>
2017-08-07 08:29:01 -0500

    Add Stetho for debug

--- app/src/main/java/com/example/max/chucknorristest/ChuckNorrisApplication.kt
+++ app/src/main/java/com/example/max/chucknorristest/ChuckNorrisApplication.kt
@@ -5,9 +5,14 @@ import com.example.max.chucknorristest.presentation.showPhrase.MainActivity
 import com.example.max.chucknorristest.presentation.showPhrase.injector.DaggerMainComponent
 import com.example.max.chucknorristest.presentation.showPhrase.injector.MainComponent
 import com.example.max.chucknorristest.presentation.showPhrase.injector.MainModule
+import com.facebook.stetho.Stetho
 
 class ChuckNorrisApplication : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+        Stetho.initializeWithDefaults(this)
+    }
 
     fun getMainComponent(activity: MainActivity): MainComponent {
         return DaggerMainComponent.builder().mainModule(MainModule(activity)).build()
